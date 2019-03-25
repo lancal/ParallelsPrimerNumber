5abe32f660a30912f5a59d6fb4359332cb899fe1
Alan <ivda.calderon@gmail.com>
2019-03-25 12:43:52 -0300

    Es primo hasta el 100.000.000 concurrent

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+//        Set <Integer> data = new ConcurrentSkipListSet<>();
+//        ForkJoinPool fjPool = new ForkJoinPool();
+//        PrimeRecursiveAction action = new PrimeRecursiveAction(data, 0, 100000000, 2);
+//        System.out.println((fjPool.invoke(action)));
+
+
+
+        long n2=IsPrime.loopMultiThread();
+        System.out.println(n2);
+
     }
 }
