7f8b8f9e8f0ba10e54d9e84a1fcd008a80c89fcb
Alan <ivda.calderon@gmail.com>
2019-03-26 09:54:29 -0300

    Ajustes hechos

--- src/com/company/IsPrime.java
+++ src/com/company/IsPrime.java
@@ -1,52 +1,38 @@
 package com.company;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class IsPrime implements Runnable {
 
-    private static final long UPPER_BOUND=100000000;
 
     long value;//the number observed
 
-    private IsPrime(long value)
+    public IsPrime(long value)
     {
         this.value=value;
     }
 
-    private boolean isAPrimeNumber()
+    @Override
+    public void run()
+    {
+        boolean result = isAPrimeNumber();
+        if(result) System.out.println("Is Prime: ["+this.value+"]");
+
+    }
+
+    public boolean isAPrimeNumber()
     {
         if(value==1 || value==2) return true;
         if(value%2L==0) return false;
-        for(long i=3;i<= value / 2;++i)
+        for(long i=3; i<= value / 2; ++i)
         {
             if(this.value%i==0) return false;
         }
+
+
+
         return true;
     }
 
-    @Override
-    /** calls isAPrimeNumber */
 
-    public void run()
-    {
-        boolean result=isAPrimeNumber();
-        if(result) System.out.println("["+this.value+"]");
-    }
-
-    /** loop from 3 to UPPER_BOUND, multithreaded */
-    public static long loopMultiThread()
-    {
-        long now = System.currentTimeMillis();
-        ExecutorService service = Executors.newFixedThreadPool(2);
-
-        for(long i=3;i< UPPER_BOUND;i+=2)
-        {
-            service.submit(new IsPrime(i));
-        }
-        service.shutdown();
-        return System.currentTimeMillis()-now;
-    }
 
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,39 @@
 package com.company;
-//import java.util.concurrent.ForkJoinPool;
-//import java.util.concurrent.ConcurrentSkipListSet;
-//import java.util.Set;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
+    private static final long UPPER_BOUND = 1000000;
+
+    private static final int NTHREADS = Runtime.getRuntime().availableProcessors();
+
     public static void main(String[] args) {
-	// write your code here
-//        Set <Integer> data = new ConcurrentSkipListSet<>();
-//        ForkJoinPool fjPool = new ForkJoinPool();
-//        PrimeRecursiveAction action = new PrimeRecursiveAction(data, 0, 100000000, 2);
-//        System.out.println((fjPool.invoke(action)));
+
+        long now = System.currentTimeMillis();
+
+        ExecutorService service = Executors.newFixedThreadPool(NTHREADS);
+
+        for(long i=2;i <= UPPER_BOUND;i++)
+        {
+            service.submit(new IsPrime(i));
+        }
+
+        service.shutdown();
+
+        long endTime = System.currentTimeMillis()-now;
+
+        long seconds = (endTime / 1000);
+
+        System.out.println("Finished in " + seconds + " seconds");
+
+        long speedup = (seconds / NTHREADS);
+
+        System.out.println("Speed-Up: "+speedup+" seconds/tasks");
 
 
-
-        long n2=IsPrime.loopMultiThread();
-        System.out.println(n2);
 
     }
 }
