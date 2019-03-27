dd9d6aa895784d314ce8bb0b1a0fd043b60238b8
lancal <thesunni174509>
2019-03-26 22:30:14 -0300

    Version Final 1.5, se comento todo el codigo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -31,32 +31,21 @@ public class Main {
 
     /**
      *
-     * Constante de tiempo maximo de espera
+     * Constante de tiempo maximo de espera.
      *
      */
     private static final int MAXTIME = 120;
 
+    /**
+     *
+     * funcion que comprueba si un numero es primo.
+     *
+     * @param value
+     * @return
+     */
+
     public static boolean isAPrimeNumber(long value) {
 
-//        if(value==1 || value==2) {
-//
-//            return true;
-//
-//        }
-//
-//        if(value%2L==0) {
-//
-//            return false;
-//
-//        }
-//
-//        for(long i=3; i<= value; ++i) {
-//
-//            if(value%i==0) return false;
-//
-//        }
-//
-//        return true;
 
         int remainder;
         for (int i = 2; i <= value ; i++) {
@@ -70,19 +59,69 @@ public class Main {
 
     }
 
+    /**
+     *
+     * Funcion main, comienza la ejecucion del programa.
+     *
+     * @param args
+     * @throws InterruptedException
+     */
+
     public static void main(String[] args) throws InterruptedException {
+
+        /**
+         *
+         * Primer for que va de 100k en 100k hasta 600k.
+         *
+         */
 
         for (int primeNumbers=(100*1000); primeNumbers <= UPPER_BOUND; primeNumbers += (100*1000)) {
 
+            /**
+             *
+             * Se declara e inicializa una variable, para guardar el tiempo de un thread.
+             *
+             */
+
             double parallelTimeT1 = 0;
+
+            /**
+             *
+             * Se imprime por pantalla 100k,200k...600k.
+             *
+             */
 
             System.out.println("Cantidad limite: " + primeNumbers);
 
+            /**
+             *
+             * Este for va desde 1 threads hasta 16 para comprobar si primo un numero.
+             *
+             */
+
             for (int cantThreads = 1; cantThreads <= NTHREADS; cantThreads++) {
+
+                /**
+                 *
+                 * Se guarda el tiempo actual en milisegundos.
+                 *
+                 */
 
                 long now = System.currentTimeMillis();
 
+                /**
+                 *
+                 * Se inicia una instancia de service con la cantidad de threads en cada pasada del for.
+                 *
+                 */
+
                 ExecutorService service = Executors.newFixedThreadPool(cantThreads);
+
+                /**
+                 *
+                 * En este for se itera cada 100k numeros, hasta 600k
+                 *
+                 */
 
                 for (int i = 2; i <= primeNumbers; i++) {
 
@@ -90,12 +129,43 @@ public class Main {
 
                 }
 
+                /**
+                 *
+                 * Service apaga todos los procesos de hilos corriento.
+                 *
+                 */
+
                 service.shutdown();
+
+                /**
+                 *
+                 * Se espera una cierta cantidad de tiempo hasta que se termine de ejecutar todos los hilos.
+                 *
+                 */
+
                 service.awaitTermination(MAXTIME, TimeUnit.SECONDS);
+
+                /**
+                 *
+                 * Se calcula el tiempo actual menos el que se obtuvo antes de empezar la ejecucion de los hilos.
+                 *
+                 */
 
                 long endTime = System.currentTimeMillis() - now;
 
+                /**
+                 *
+                 * Se guarda el tiempo calculdo y se guarda en otra variable.
+                 *
+                 */
+
                 double parallelTime = (double) endTime;
+
+                /**
+                 *
+                 * En este if se guarda el tiempo de ejecucion de 1 hilo para luego calcular los speed-up
+                 *
+                 */
 
                 if (cantThreads == 1) {
 
@@ -103,9 +173,21 @@ public class Main {
 
                 }
 
+                /**
+                 *
+                 * Se calcula el Speed-up.
+                 *
+                 */
+
                 double speedup = parallelTimeT1 / parallelTime;
 
-                System.out.println("Hilos " + cantThreads + " Tiempo: " + parallelTime + " milisegundo Speed-Up: " + speedup + " segundos por hilos");
+                /**
+                 *
+                 * Se imprime la cantidad de threads y el tiempo empleado en cada hilo y se imprime el speed-up calculado
+                 *
+                 */
+
+                System.out.println("Threads: " + cantThreads + " Time: " + parallelTime + " miliseconds, Speed-Up: " + speedup);
 
             }
 
--- src/com/company/isPrime.java
+++ src/com/company/isPrime.java
@@ -1,27 +1,39 @@
 package com.company;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-
+//import java.util.concurrent.atomic.AtomicInteger;
 
 public class isPrime implements Runnable {
 
-    private static final AtomicInteger PRIME = new AtomicInteger(0) ;
+    //private static final AtomicInteger PRIME = new AtomicInteger(0) ;
 
-    private long value;
+    /**
+     *
+     * Se declara e inicializa la variable value.
+     *
+     */
+
+    private long value = 0;
+
+    /**
+     *
+     * Metodo para guardar un numero, en este caso primo.
+     *
+     * @param value
+     */
 
     public isPrime(long value) {
 
         this.value = value;
     }
 
+    /**
+     *
+     * Ejecucion de la funcion Main para encontrar si un numero es primo.
+     *
+     */
+
     @Override
     public void run() {
-
-//        if(result) {
-//
-//            PRIME.accumulateAndGet();
-//        }
 
         Main.isAPrimeNumber(this.value);
 
